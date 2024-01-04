5bfffd2c2415a1bc0d890995e18b0859c850d4ce
nour-nono <115364839+nour-nono@users.noreply.github.com>
2024-01-05 00:17:44 +0200

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #script that accepts one argument and pass it to a regular expression matching
 #must match School
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
