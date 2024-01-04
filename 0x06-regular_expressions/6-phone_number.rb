ad3dffbf5e830c6094a0cd5ee7e357d83d727887
nono <nour98nour@gmail.com>
2024-01-04 10:49:55 -0800

    6 task

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #script that accepts one argument and pass it to a regular expression matching
 #must match School
-puts ARGV[0].scan(/\d*/).join
+puts ARGV[0].scan(/\A\d{1,}\z/).join
