d40f3c724285c0df3e46c7bcdca873b5e7e3cd22
nono <nour98nour@gmail.com>
2024-01-04 10:40:53 -0800

    4 task

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #script that accepts one argument and pass it to a regular expression matching
 #must match School
-puts ARGV[0].scan(/hbt{,4}n/).join
+puts ARGV[0].scan(/hbt*n/).join
