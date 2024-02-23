524175439916c8f7ad080431b1d399cdda14899d
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-02-23 20:49:24 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure   => installed,
+}
+
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
