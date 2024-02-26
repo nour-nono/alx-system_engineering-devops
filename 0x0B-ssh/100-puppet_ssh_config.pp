22063e6ab9ab6f2f6ac9775eb6d8b2ecaec2b022
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-02-26 02:04:23 +0200

    Update 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+file { '/home/ubuntu/.ssh/config':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+}
