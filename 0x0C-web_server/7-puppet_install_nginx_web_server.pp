56c10760e71f786a1a0df1db682f161afda6aea3
nono <nour98nour@gmail.com>
2024-02-27 14:58:48 -0800

    puppet task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,44 @@
+class nginx_setup {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    require    => Package['nginx'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => '
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  root /var/www/html;
+
+  index index.html;
+
+  server_name _;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+  location /redirect_me {
+    return 301 https://www.example.com;
+  }
+}',
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+  }
+}
+
+include nginx_setup
