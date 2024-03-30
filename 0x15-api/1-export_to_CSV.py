ea03affe42964928257011c42fbccd6e0af0360a
nono <nour98nour@gmail.com>
2024-03-30 06:59:34 +0200

    added documentation to all files

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+"""Python script that, using this REST API, for a given employee ID
+and saves it to a CSV file."""
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,2 @@
 #!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
