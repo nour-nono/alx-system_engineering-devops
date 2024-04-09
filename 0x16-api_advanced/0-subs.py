b62ecb32d4bc570fae2f473ad8faeb16a6812af8
nono <nour98nour@gmail.com>
2024-04-09 22:01:23 +0200

    fix 0 task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,6 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about/.json"
     x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
     if x.status_code == 200:
-        print(x.json()['data']['subscribers'])
+        return x.json()['data']['subscribers']
     else:
         return 0
