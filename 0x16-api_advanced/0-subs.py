119d026428a64d470f38f106966eea8a45ce2a02
nono <nour98nour@gmail.com>
2024-04-09 23:30:43 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Module that queries the Reddit API and returns the number of subscribers """
+""" Module that queries the Reddit API and
+returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """get number of subscribers"""
-
-    headers = {'User-Agent': 'MyPythonScript/1.0'}
-    request = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
-
-    if request.status_code == 200:
-        subscribers = request.json().get('data').get('subscribers')
-        return subscribers if subscribers else 0
-    return 0
+    if subreddit is None:
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
+    x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
+    if x.status_code == 200:
+        try:
+            return x.json()['data']['subscribers']
+        except KeyError:
+            return 0
+    else:
+        return 0
