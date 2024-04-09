2e96df65a16a0a27ec948bd733bc50cbbfc7530b
nono <nour98nour@gmail.com>
2024-04-09 23:33:48 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 """ Module that queries the Reddit API and prints the titles 
 of the first 10 hot posts listed for a given subreddit. """
+import requests
+
+
 def top_ten(subreddit):
-    url = 
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
+    x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
+    if x.status_code == 200:
+        try:
+            for i in range(10):
+                print(x.json()['data']['children'][i]['data']['title'])
+        except KeyError:
+            print(None)
+    else:
+        print(None)
