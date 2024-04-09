0b6dd009acea59d0641723c5291be17b69029562
nono <nour98nour@gmail.com>
2024-04-09 21:59:54 +0200

    fix 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,10 @@ import requests
 import sys
 def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
-        print(0)
-        return
+        return 0
     url = f"https://www.reddit.com/r/{subreddit}/about/.json"
     x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
     if x.status_code == 200:
         print(x.json()['data']['subscribers'])
     else:
-        print(0)
+        return 0
